cbc1445c78d64bf889677f7363fd4594f0a78ffb
CrazyHamburger7 <andrii.boiko2008@gmail.com>
2024-11-20 20:39:43 +0200

    Add files via upload

--- src/js/Eco help.js
+++ src/js/Eco help.js
@@ -8,14 +8,13 @@ function changeLanguage() {
     document.querySelectorAll(".nav-links a")[0].innerHTML = isEnglish ? "Our telegram" : "Наш телеграм";
     document.querySelectorAll(".nav-links a")[1].innerHTML = isEnglish ? "Home" : "Головна";
     document.querySelectorAll(".nav-links a")[2].innerHTML = isEnglish ? "Pages" : "Сторінки";
-    document.querySelectorAll(".nav-links a")[3].innerHTML = isEnglish ? "History of Kharkiv" : "Історія Харкова";
-    document.querySelectorAll(".nav-links a")[4].innerHTML = isEnglish ? "Ecological Problems of Kharkiv" : "Екологічні проблеми Харкова";
-    document.querySelectorAll(".nav-links a")[5].innerHTML = isEnglish ? "Waste Status and Recycling" : "Стан та переробка відходів";
-    document.querySelectorAll(".nav-links a")[6].innerHTML = isEnglish ? "Air and Its Quality" : "Повітря та його якість";
-    document.querySelectorAll(".nav-links a")[7].innerHTML = isEnglish ? "Cleanliness of Water Bodies in Kharkiv" : "Чистота водойм Харкова";
-    document.querySelectorAll(".nav-links a")[8].innerHTML = isEnglish ? "State of Green Zones" : "Стан зелених зон";
-    document.querySelectorAll(".nav-links a")[9].innerHTML = isEnglish ? "Volunteering" : "Еко-волонтерство";
-    document.querySelectorAll(".nav-links a")[10].innerHTML = isEnglish ? "Tips for preserving the environment" : "Поради щодо збереження довкілля";
+    document.querySelectorAll(".nav-links a")[3].innerHTML = isEnglish ? "Ecological Problems of Kharkiv" : "Екологічні проблеми Харкова";
+    document.querySelectorAll(".nav-links a")[4].innerHTML = isEnglish ? "Waste Status and Recycling" : "Стан та переробка відходів";
+    document.querySelectorAll(".nav-links a")[5].innerHTML = isEnglish ? "Air and Its Quality" : "Повітря та його якість";
+    document.querySelectorAll(".nav-links a")[6].innerHTML = isEnglish ? "Cleanliness of Water Bodies in Kharkiv" : "Чистота водойм Харкова";
+    document.querySelectorAll(".nav-links a")[7].innerHTML = isEnglish ? "State of Green Zones" : "Стан зелених зон";
+    document.querySelectorAll(".nav-links a")[8].innerHTML = isEnglish ? "Volunteering" : "Еко-волонтерство";
+    document.querySelectorAll(".nav-links a")[9].innerHTML = isEnglish ? "Tips for preserving the environment" : "Поради щодо збереження довкілля";
 
     // Зміна заголовка для секції "Еко-волонтерство"
     document.getElementById("section-title").innerText = isEnglish ? "What is eco-volunteering?" : "Що таке еко-волонтерство?";
--- src/js/news.js
+++ src/js/news.js
@@ -1,10 +1,11 @@
 function changeLanguage() {
-    const lang = document.getElementById("lang-btn").innerText;
-  
-    // Toggle the language
-    if (lang === "English") {
+  const langBtn = document.getElementById("lang-btn");
+  const lang = langBtn.innerText;
+
+  // Toggle the language
+  if (lang === "English") {
       // Change to English
-      document.getElementById("lang-btn").innerText = "Українська";
+      langBtn.innerText = "Українська";
       document.getElementById("title").innerText = "Ecology of Kharkiv";
       document.getElementById("eco-title").innerText = "Water resources of Kharkiv";
       document.getElementById("eco-content").innerText = 
@@ -12,31 +13,28 @@ function changeLanguage() {
       document.getElementById("war-title").innerText = "The war and its impact on Kharkiv's reservoirs";
       document.getElementById("war-content").innerText = 
         "The ongoing war in eastern Ukraine is having a serious impact not only on human lives but also on the environment. Kharkiv, as one of the largest cities in the region, has undergone significant changes due to hostilities, which have caused damage not only to infrastructure but also to water resources. At the same time, the environmental consequences of the war pose a real threat to the health of the population and the sustainable development of the city.";
-      
-      // Change navigation links to English
+      document.getElementById("war2-content").innerText = "As a result of the fire after the Russian attack on the oil depot in the Nemyshlyansky district of Kharkiv, oil products entered the Nemyshlya River and further downstream into the Lopan, Udy, and Kharkiv rivers. This was reported by the State Environmental Inspectorate in the Kharkiv region. In order to respond to the situation, the State Environmental Inspectorate in the Kharkiv region, within its competence, formed a group of state environmental inspectors of the Kharkiv region, which went to the scene of the incident to the water bodies to record the facts of the presence of the specified oil slick and take water samples. The State Environmental Inspectorate commented on the pollution of the Kharkiv rivers with oil products. At the time of the trip to the Udy River in the area of ​​the villages of Khorosheve and Temnivka, a significant oil slick was discovered, which continues to move downstream. In order to eliminate the emergency situation related to the pollution of water resources in the Kharkiv region and the city of Kharkiv, the inspection informed the Kharkiv Regional Military Administration and the Main Directorate of the State Emergency Service of Ukraine in the Kharkiv region. The Kharkiv Regional Prosecutor's Office was also informed about the situation. As the inspection emphasized, within the scope of its competence, \"all necessary measures continue to be taken. We are conducting laboratory tests of the selected samples, based on the results of which appropriate inspection response measures will be taken.\"";
+      document.getElementById("war2-title").innerText="Pollution of Kharkiv rivers with petroleum products";
+
       const navLinks = document.querySelectorAll(".nav-links a");
       const englishTexts = [
-        "Our telegram",
-        "Home",
-        "Pages",
-        "History of Kharkiv",
-        "Environmental problems of Kharkiv",
-        "Waste Status and Recycling",
-        "Air and Its Quality",
-        "State of Green Zones",
-        "Volunteering",
-        "Tips for Environmental Conservation",
-        "Eco-volunteering",
+          "Our telegram",
+          "Home",
+          "Pages",
+          "Environmental problems of Kharkiv",
+          "Waste Status and Recycling",
+          "Air and Its Quality",
+          "State of Green Zones",
+          "Volunteering",
+          "Tips for Environmental Conservation",
+          "Eco-volunteering",
       ];
       navLinks.forEach((link, index) => {
-        link.innerText = englishTexts[index];
+          link.innerText = englishTexts[index];
       });
-  
-    
-  
-    } else {
+  } else {
       // Change back to Ukrainian
-      document.getElementById("lang-btn").innerText = "English";
+      langBtn.innerText = "English";
       document.getElementById("title").innerText = "Екологія Харкова";
       document.getElementById("eco-title").innerText = "Водні ресурси Харкова";
       document.getElementById("eco-content").innerText = 
@@ -44,29 +42,28 @@ function changeLanguage() {
       document.getElementById("war-title").innerText = "Війна та її вплив на водойми Xаркова";
       document.getElementById("war-content").innerText = 
         "Війна, що триває на сході України, має серйозний вплив не тільки на людські життя, але й на навколишнє середовище. Харків, як одне з найбільших міст регіону, зазнав значних змін через бойові дії, що спричинили шкоду не тільки інфраструктурі, але й водним ресурсам. Водночас, екологічні наслідки війни становлять реальну загрозу для здоров’я населення та сталого розвитку міста.";
-      
-      // Change navigation links to Ukrainian
+      document.getElementById("war2-content").innerText="У результаті пожежі після російської атаки на нафтобазу в Немишлянському районі Харкова відбулось потрапляння нафтопродуктів в річку Немишля та далі за течією в річки Лопань, Уди, Харків. Про це повідомляє Державна екологічна інспекція у Харківській області. З метою реагування на ситуацію Державною екологічною інспекцією у Харківській області, в межах компетенції, була сформована група зі складу державних інспекторів з охорони навколишнього природного середовища Харківської області, яка виїхала на місце події до водних об’єктів для фіксації фактів наявності вказаної нафтової плями та відбору проб води. У Держекоінспекції прокоментували забруднення річок Харкова нафтопродуктами. На момент виїзду на річку Уди в районі села Хорошеве та села Темнівка виявлено нафтову пляму значних розмірів, яка продовжує рухатись за течією. З метою ліквідації надзвичайної ситуації, пов’язаної із забрудненням водних ресурсів Харківської області та міста Харкова, інспекцією проінформовано Харківську обласну військову адміністрацію та ГУ ДСНС України у Харківській області. Про вказану ситуацію також проінформовано Харківську обласну прокуратуру. Як підкреслили в інспекції, в межах компетенції, «продовжується вживання всіх необхідних заходів. Здійснюємо лабораторні дослідження відібраних проб, по результатам яких буде вжито відповідних заходів інспекційного реагування».";
+      document.getElementById("war2-title").innerText="Забруднення річок Харкова нафтопродуктами";
+
       const navLinks = document.querySelectorAll(".nav-links a");
       const ukrainianTexts = [
-        "Наш телеграм",
-        "Головна",
-        "Сторінки",
-        "Історія Харкова",
-        "Екологічні проблеми Харкова",
-        "Статус та переробка відходів",
-        "Повітря та його якість",
-        "Стан зелених зон",
-        "Допомога",
-        "Поради щодо збереження довкілля",
-        "Еко-волонтерство",
+          "Наш телеграм",
+          "Головна",
+          "Сторінки",
+          "Екологічні проблеми Харкова",
+          "Статус та переробка відходів",
+          "Повітря та його якість",
+          "Стан зелених зон",
+          "Допомога",
+          "Поради щодо збереження довкілля",
+          "Еко-волонтерство",
       ];
       navLinks.forEach((link, index) => {
-        link.innerText = ukrainianTexts[index];
+          link.innerText = ukrainianTexts[index];
       });
-  
-      // Change the video to Ukrainian
-    }
   }
+}
+
   window.addEventListener('scroll', () => {
 const scrollToTop = document.querySelector('.scrollToTop');
 if (window.scrollY > 200) {
